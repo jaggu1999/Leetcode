cfa676dc6fcc64e826bdddbb38dc06a0c507157c
Jagadeesh <ch.jagadeeshkumarc@gmail.com>
2023-01-23 12:25:52 +0530

    Added Single Number

---
+++ SingleNumber.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (auto val : nums) {
+            ans ^= val;
+        }
+        return ans;
+    }
+};
