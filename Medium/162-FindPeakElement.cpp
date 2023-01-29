43b2f2fb79031c46adb48c6b9293acf9ff4322fc
Jagadeesh <ch.jagadeeshkumarc@gmail.com>
2023-01-29 23:46:02 +0530

    Added Find Peak Element

---
+++ Medium/162-FindPeakElement.cpp
@@ -0,0 +1,21 @@
+/**
+Question Name: 162. Find Peak Element
+Author: Jagadeesh Kumar Ch
+Difficulty: Medium
+Question Link: https://leetcode.com/problems/find-peak-element/
+*/
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int len = nums.size(), low = 0, high = len - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            }
+            else high = mid;
+        }
+        return low;
+    }
+};
